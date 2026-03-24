92144e7b30b0dcd110ca3caedea4467bda83e97c
ROSMS1 <narfel204@gmail.com>
2026-03-24 03:58:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # Remplacez ceci par l'ID de votre feuille Google Sheets (se trouve dans l'URL)
 SHEET_ID = '1RWQSre19ZmQl4rzxNcsIwvWlJuu_Nm9RMBKbBwPuYyE'
-SHEET_NAME = 'Feuille1'
+SHEET_NAME = 'MaBaseEloquence'
 URL = https://docs.google.com/spreadsheets/d/1RWQSre19ZmQl4rzxNcsIwvWlJuu_Nm9RMBKbBwPuYyE/edit?usp=sharing
 
 st.set_page_config(page_title="L'Art de l'Éloquence", page_icon="⚖️")
